6736374df6cbe0cc69d31983fa1b3d1b7f057dc4
Charles Pick <charles.pick@gmail.com>
2013-03-31 20:17:34 +0100

    Added tag v2.1.0

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function(){
   var jQuery = require('jquery');
   /* ============================================================
-   * bootstrap-button.js v2.0.4
+   * bootstrap-button.js v2.1.0
    * http://twitter.github.com/bootstrap/javascript.html#buttons
    * ============================================================
    * Copyright 2012 Twitter, Inc.
